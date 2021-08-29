6d11c8f928f1289876e87ea201d65dcad2f7e40d
Join-Red-Army <this.is.my.address.for.mail@ya.ru>
2021-08-30 00:55:10 +0300

    добавлена мета информация в head

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -8,7 +8,8 @@ const Button = (props) => {
   return (
     <div className={`button button-${buttonIndex}`} 
     // onClick={(ev) => onClickButton(ev, buttonIndex)}
-    onPointerUp={(ev) => onClickButton(ev, buttonIndex)}
+    // onPointerUp={(ev) => onClickButton(ev, buttonIndex)}
+    onPointerDown={(ev) => onClickButton(ev, buttonIndex)}
     >
     </div>
   );
